8625f634f6aeb849a03ceb86ffbf6c875580fcb4
Filipe Mei <peticormei@gmail.com>
2019-11-14 11:08:52 -0300

    adicionando controle do robo pela segunda camada do robo

--- Project/src/controller_controller_c/controller_controller_types.c
+++
@@ -1,9 +0,0 @@
-/* --- Generated the 13/11/2019 at 14:15 --- */
-/* --- heptagon compiler, version 1.03.00 (compiled sun. oct. 20 0:47:43 CET 2019) --- */
-/* --- Command line: /home/opam/.opam/4.02/bin/heptc -target c controller_controller.ept --- */
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include "controller_controller_types.h"
-
